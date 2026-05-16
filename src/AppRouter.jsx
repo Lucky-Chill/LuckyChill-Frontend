74c3a78095c38b888333724a01fd78aa5fd544a4
rlown <128786877+rlown@users.noreply.github.com>
2026-05-16 21:45:56 +0900

    [#9] chore : 임포트 수정

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/login/loginPage';
+import LoginPage from './pages/login/LoginPage';
 
 function AppRouter() {
   return (
