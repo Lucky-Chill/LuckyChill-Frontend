ee3da7ed0bb8305c3e41eb346267c7551bbe500a
˙𐃷˙ <sodang0645@gmail.com>
2026-05-16 22:12:50 +0900

    [#14] feat : 이미지 추가

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
+import ReservationPage from './features/reservation/ReservationPage';
 
 function AppRouter() {
   return (
@@ -7,6 +8,7 @@ function AppRouter() {
       <Route path='/' element={<LoginPage />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='*' element={<Navigate to='/' replace />} />
+      <Route path='/reservation' element={<ReservationPage />} />
     </Routes>
   );
 }
